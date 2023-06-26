b2eb208b37e951fa1896a79f2e2261cd808f5445
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-06-26 19:42:39 -0300

    enviando emails pelo django

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
-def post_list(requets):
-    posts = Post.published.all()
-    return render(requets,'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html' 
+    
+    
+'''def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    
+    return render(request,'blog/post/list.html', {'page': page, 'posts': posts})'''
 
 
 def post_detail(request, year, month, day, post):
@@ -12,3 +32,29 @@ def post_detail(request, year, month, day, post):
                              publish__month=month, publish__day=day)
     
     return render(request, 'blog/psot/detail.html', {'post': post})
+
+
+def post_shere(request, post_id):
+    post = get_object_or_404(Post, id= post_id, status = 'published')
+    send = False
+    
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read"\
+                f"{post.title}"
+            message = f"Read {post.title} at {post.url}\n\n"\
+                f"{cd['name']} \'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@mysite.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+        
+        return render(request, 'blog/post/share.html', {'post': post,
+                                                        'form': form,
+                                                        'sent': sent})
+            
+    
+    
